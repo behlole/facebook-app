95925e66f0cbb67e0275e48356a0056c575a525f
behlole <behloleaqil@gmail.com>
2023-05-12 21:02:13 -0400

    completed header and it's custom styling

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,23 +5,31 @@ import HeaderIcon from "@/components/HeaderIcon";
 
 function Header() {
     return (
-        <div>
+        <div className={"sticky top-0 z-50 bg-white flex items-center p-2 lg:px-x-5 shadow-md"}>
             <div className={"flex items-center"}>
                 <Image src="https://links.papareact.com/5me" width={40} height={40} layout={"fixed"}/>
                 <div className={"flex ml-2 items-center rounded-full bg-gray-100 p-2"}>
                     <SearchIcon className={"h-6 text-gray-600"}/>
                     <input type={"text"} placeholder={"Search Facebook"}
-                           className={"flex ml-2 items-center bg-transparent outline-none"}/>
+                           className={"hidden md:inline-flex  flex-shrink ml-2 items-center bg-transparent outline-none"}/>
                 </div>
             </div>
             <div className={"flex justify-center flex-grow"}>
-                <div>
-                    <HeaderIcon Icon={HomeIcon}/>
-                    <HeaderIcon Icon={FlagIcon}/>
-                    <HeaderIcon Icon={PlayIcon}/>
-                    <HeaderIcon Icon={ShoppingCartIcon}/>
-                    <HeaderIcon Icon={UserGroupIcon}/>
+                <div className={"flex space-x-6 md:space-x-2"}>
+                    <HeaderIcon Icon={HomeIcon} active={true}/>
+                    <HeaderIcon Icon={FlagIcon} active={false}/>
+                    <HeaderIcon Icon={PlayIcon} active={false}/>
+                    <HeaderIcon Icon={ShoppingCartIcon} active={false}/>
+                    <HeaderIcon Icon={UserGroupIcon} active={false}/>
                 </div>
+            </div>
+            <div className={"flex items-center sm:space-x-2 justify-end"}>
+                {/*<Image src={} alt={}/>*/}
+                <p className={"whitespace-nowrap font-semibold pr-3 "}>Behlole Aqil</p>
+                <ViewGridIcon className={"icon"}/>
+                <ChatIcon className={"icon"}/>
+                <BellIcon className={"icon"}/>
+                <ChevronDownIcon className={"icon"}/>
             </div>
         </div>
     );
--- components/HeaderIcon.jsx
+++ components/HeaderIcon.jsx
@@ -1,7 +1,9 @@
-function HeaderIcon({Icon}) {
+function HeaderIcon({Icon, active}) {
     return (
-        <div>
-            <Icon className={"text-center h-5"}/>
+        <div
+            className={"flex items-center cursor-pointer md:px-10 sm:h-14 md:hover:bg-gray-100 rounded-xl active:border-b-2 active:border-blue-500 group"}>
+            <Icon
+                className={`h-5 text-gray-500 text-center group-hover:text-blue-500 sm:h-7 mx-auto ${active && 'text-blue-500'}`}/>
         </div>
     );
 }
